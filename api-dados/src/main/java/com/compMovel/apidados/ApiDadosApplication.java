38df5acc7bc11b4be323992531219c47c90c6e2a
Pedro Arthur Freitas <pedro.afd@discente.ufma.br>
2022-10-18 01:01:22 -0300

    initial commit

---
+++ api-dados/src/main/java/com/compMovel/apidados/ApiDadosApplication.java
@@ -0,0 +1,13 @@
+package com.compMovel.apidados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDadosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDadosApplication.class, args);
+    }
+
+}
